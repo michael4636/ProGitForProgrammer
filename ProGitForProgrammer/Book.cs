5fc7010bb932a2774bb763a563dd27b528d03719
michael4636 <michael4636@gmail.com>
2024-03-26 11:37:27 +0800

    20240226

---
+++ ProGitForProgrammer/Book.cs
@@ -0,0 +1,10 @@
+using System;
+namespace progitforprogrammers
+{
+    class book
+    { 
+        public string Title { get; set; }
+        public list<string> authors { get; set; }
+        public datetime oublicationdate { get; set; 
+        ]}
+}
